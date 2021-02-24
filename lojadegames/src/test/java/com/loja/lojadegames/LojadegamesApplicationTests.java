01011407c685cb12c8b17d62f37affe04c3e521d
Raphael Scaziti <rscaziti@gmail.com>
2021-02-24 09:57:08 -0300

    Adicionado arquivo do Spring Initialzr

---
+++ lojadegames/src/test/java/com/loja/lojadegames/LojadegamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loja.lojadegames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojadegamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
