2ae9534545a0a9bc7bcb4236eb9c29894785612c
Raphael Scaziti <rscaziti@gmail.com>
2021-02-24 14:30:24 -0300

    Adicionado método DELETE na Controller

--- lojadegames/src/main/java/com/loja/lojadegames/controller/ProdutoController.java
+++ lojadegames/src/main/java/com/loja/lojadegames/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,5 +50,11 @@ public class ProdutoController
 	public ResponseEntity<ProdutoLojaDeGames> put(@RequestBody ProdutoLojaDeGames produto)
 	{
 		return ResponseEntity.status(HttpStatus.OK).body(this.produto.save(produto));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id)
+	{
+		this.produto.deleteById(id);
 	}
 }
