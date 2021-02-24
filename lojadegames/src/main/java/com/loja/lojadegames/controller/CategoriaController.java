466650fbec51001d2ebb7c03daba01039136c7ed
Raphael Scaziti <rscaziti@gmail.com>
2021-02-24 10:59:46 -0300

    Adicionado método delete na controller

--- lojadegames/src/main/java/com/loja/lojadegames/controller/CategoriaController.java
+++ lojadegames/src/main/java/com/loja/lojadegames/controller/CategoriaController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +18,7 @@ import com.loja.lojadegames.model.CategoriaLojaDeGames;
 import com.loja.lojadegames.repository.CategoriaLojaDeGamesRepository;
 
 @RestController
-@RequestMapping("/categoria")
+@RequestMapping("/categorialojadegames")
 @CrossOrigin("*")
 public class CategoriaController 
 {
@@ -41,5 +43,17 @@ public class CategoriaController
 	public ResponseEntity<CategoriaLojaDeGames> post(@RequestBody CategoriaLojaDeGames categoria)
 	{
 		return ResponseEntity.status(HttpStatus.CREATED).body(this.categoriaLoja.save(categoria));
+	}
+	
+	@PutMapping
+	public ResponseEntity<CategoriaLojaDeGames> put(@RequestBody CategoriaLojaDeGames categoria)
+	{
+		return ResponseEntity.status(HttpStatus.OK).body(this.categoriaLoja.save(categoria));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id)
+	{
+		this.categoriaLoja.deleteById(id);
 	}
 }
